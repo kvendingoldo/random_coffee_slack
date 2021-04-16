6fa7c0ce75ccb01159054fe1e63bb48f7fd9b59a
Alexander Sharov <kvendingoldo@yandex.ru>
2021-04-16 21:01:20 +0400

    Migrate to connection pool

--- src/database/interface/connector.py
+++ src/database/interface/connector.py
@@ -1,14 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from loguru import logger
+
+
 class Connector(object):
-    def __init__(self, db):
-        self.db = db
+    def __init__(self, connection_pool):
+
+        self.connection = connection_pool.get_connection()
+
+        if self.connection.is_connected():
+            info = self.connection.get_server_info()
+            logger.info("Connected to MySQL database using connection pool ...")
+            logger.info(f"Connected to MySQL Server version {info}")
+            cursor = self.connection.cursor()
+            cursor.execute("select database();")
+            record = cursor.fetchone()
+            logger.info(f"You're connected to database: {record}")
+
+    def __del__(self):
+        if self.connection.is_connected():
+            self.connection.close()
+            logger.info(f"MySQL connection is closed")
 
     def get(self, sql_statement):
-        with self.db.cursor(buffered=True) as cursor:
+        with self.connection.cursor(buffered=True) as cursor:
             cursor.execute(sql_statement)
-            self.db.commit()
+            self.connection.commit()
             return cursor.fetchall()[0]
 
     def post(self, sql_statement):
-        with self.db.cursor(buffered=True) as cursor:
+        with self.connection.cursor(buffered=True) as cursor:
             cursor.execute(sql_statement)
-            return self.db.commit()
+            return self.connection.commit()
