6c4306a47e6b7f185e3743947164aec8b10c8bf6
greatweirdening <nero436@gmail.com>
2021-04-16 19:55:18 +0300

    Enhancements

--- src/database/dao/users.py
+++ src/database/dao/users.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from entities import user
+from src import entities
 
 
 def add(connector, user):
@@ -12,13 +12,13 @@ def add(connector, user):
     return connector.post(sql_statement)
 
 
-def is_new(connector, user):
+def is_new(connector, uid):
     sql_statement = ""
     return connector.get(sql_statement)
 
 
-def is_ready(connector, user):
-    sql_statement = f"SELECT ready FROM users WHERE username = \'{user}\'"
+def is_ready(connector, uid):
+    sql_statement = f"SELECT ready FROM users WHERE username = \'{uid}\'"
 
     return connector.get(sql_statement)
 
@@ -32,17 +32,17 @@ def get_user(connector, uid):
     sql_statement = f"SELECT * FROM users WHERE uid = \'{uid}\'"
     result = connector.get(sql_statement)
 
-    return user.User(username=result[1], uid=result[2], ready=result[3], aware=result[4])
+    return entities.user.User(username=result[1], uid=result[2], ready=result[3], aware=result[4])
 
 
-def list(connector):
+def list_all(connector):
     users = []
 
     sql_statement = f"SELECT * FROM users"
     result = connector.get(sql_statement)
     for row in result:
         users.append(
-            user.User(username=row[1], uid=row[2], ready=row[3], aware=row[4])
+            entities.user.User(username=row[1], uid=row[2], ready=row[3], aware=row[4])
         )
 
     return users
