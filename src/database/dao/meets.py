cc919284685e62e0a63c272dbe0c64f2d098dd3f
greatweirdening <nero436@gmail.com>
2021-04-16 19:50:46 +0300

    DB enhancements(fix)

--- src/database/dao/meets.py
+++ src/database/dao/meets.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
-def add_meet(connector, user1, user2):
+def add(connector, user1, user2):
     sql_statement = f"INSERT IGNORE INTO meets (uid1, uid2) VALUES " \
-                    f"(\"{user1.uid}\", " \
-                    f"\"{user2.uid}\" "
+                    f"(\'{user1.uid}\', " \
+                    f"\'{user2.uid}\' "
 
     return connector.post(sql_statement)
 
-def get_meet(connector, user1, user2, season):
-    sql_statement = f"SELECT * FROM users WHERE season = \"{season}\" AND(uid1 = \"{user1}\" OR uid2 = \"{user2}\")"
+def get(connector, user1, user2, season):
+    sql_statement = f"SELECT * FROM meets WHERE season = \'{season}\' AND(uid1 = \'{user1}\' OR uid2 = \'{user2}\')"
 
     return connector.get(sql_statement)
--- src/database/dao/users.py
+++ src/database/dao/users.py
@@ -4,10 +4,10 @@ from entities import user
 
 def add(connector, user):
     sql_statement = f"INSERT IGNORE INTO users (username, uid, ready, aware) VALUES " \
-                    f"(\"{user.username}\", " \
-                    f"\"{user.uid}\", " \
-                    f"\"{int(user.ready)}\", " \
-                    f"\"{int(user.aware)}\")"
+                    f"(\'{user.username}\', " \
+                    f"\'{user.uid}\', " \
+                    f"\'{int(user.ready)}\', " \
+                    f"\'{int(user.aware)}\')"
 
     return connector.post(sql_statement)
 
@@ -18,18 +18,18 @@ def is_new(connector, user):
 
 
 def is_ready(connector, user):
-    sql_statement = f"SELECT ready FROM users WHERE username = \"{user}\""
+    sql_statement = f"SELECT ready FROM users WHERE username = \'{user}\'"
 
     return connector.get(sql_statement)
 
 
 def set_ready(connector, uid):
-    sql_statement = f"UPDATE users SET ready = \"{int(uid.ready)}\" WHERE uid = \"{uid.uid}\""
+    sql_statement = f"UPDATE users SET ready = \'{int(uid.ready)}\' WHERE uid = \'{uid.uid}\'"
     return connector.post(sql_statement)
 
 
 def get_user(connector, uid):
-    sql_statement = f"SELECT * FROM users WHERE uid = \"{uid}\""
+    sql_statement = f"SELECT * FROM users WHERE uid = \'{uid}\'"
     result = connector.get(sql_statement)
 
     return user.User(username=result[1], uid=result[2], ready=result[3], aware=result[4])
