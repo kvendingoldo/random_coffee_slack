e39c3dfbd2a20664c53aa5d25bb8684d8fa93855
Alexander Sharov <kvendingoldo@yandex.ru>
2021-09-03 23:35:37 +0400

    fix(database): Add __check_connection function

--- src/database/interface/connector.py
+++ src/database/interface/connector.py
@@ -5,7 +5,7 @@ from loguru import logger
 
 class Connector:
     def __init__(self, connection_pool):
-
+        self.connection_pool = connection_pool
         self.connection = connection_pool.get_connection()
 
         if self.connection.is_connected():
@@ -17,18 +17,29 @@ class Connector:
             record = cursor.fetchone()
             logger.info(f"Connected to database: {record}")
 
+    def __check_connection(self):
+        if not self.connection.is_connected():
+            self.connection = self.connection_pool.get_connection()
+            logger.info("MySQL connection updated to new one from connection pool")
+        else:
+            logger.info("MySQL connection is fine")
+
     def __del__(self):
         if self.connection.is_connected():
             self.connection.close()
             logger.info("MySQL connection is closed")
 
     def get(self, sql_statement):
+        self.__check_connection()
+
         with self.connection.cursor(buffered=True) as cursor:
             cursor.execute(sql_statement)
             self.connection.commit()
             return cursor.fetchall()
 
     def post(self, sql_statement):
+        self.__check_connection()
+
         with self.connection.cursor(buffered=True) as cursor:
             cursor.execute(sql_statement)
             return self.connection.commit()
