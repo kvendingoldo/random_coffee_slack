37ef2a7663d5d1e0cee7e5bb06464b08ac4c37c8
Андрей Череваткин <andy.v.che@gmail.com>
2022-10-02 22:58:58 +0200

    fix: adjust alerting logs filter (#5)

--- admin-tools/alerting.sh
+++ admin-tools/alerting.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null
 source vars
-errors=$(docker logs bot 2>&1|grep "$(date -d '1 day ago' +%Y-%m-%d)"|grep ERROR)
+errors=$(docker logs bot 2>&1|grep "$(date -d '1 day ago' +%Y-%m-%d)"|grep -i 'ERROR|Exception')
 curl -X POST -d "{\"text\": \"${errors}\"}" "${SLACK_WEBHOOK}"
