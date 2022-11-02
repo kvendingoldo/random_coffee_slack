c2f38563657e743f1e94beb5f5f759d3c050950e
Alexander Sharov <kvendingoldo@yandex.ru>
2022-11-02 16:25:45 +0100

    fix: env variables

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 import os
-import environ
 import time
 import yaml
 
@@ -13,8 +12,8 @@ def load(yaml_path):
     with open(yaml_path, "r") as file:
         config = yaml.safe_load(file)
 
-    slack_bot_token = os.environ.get("SLACK_BOT_TOKEN")
-    slack_app_token = os.environ.get("SLACK_APP_TOKEN")
+    slack_bot_token = os.environ["SLACK_BOT_TOKEN"]
+    slack_app_token = os.environ["SLACK_APP_TOKEN"]
 
     db_password = os.environ.get("DATABASE_PASSWORD")
 
